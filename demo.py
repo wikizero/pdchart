c56b552e1cac8b17acd79c26cade01a572eef2e1
attitudes <937316808@qq.com>
2019-06-01 20:11:58 +0800

    support more charts

--- demo.py
+++ demo.py
@@ -2,15 +2,89 @@
 
 import pandas as pd
 import pdchart
+from pyecharts.globals import ThemeType
+from pyecharts import options as opts
+from pyecharts.charts import Grid
 
 f = pd.DataFrame({
     'pig': [60, 180, 489, 675, 1776],
-    'horse': [49, 250, 281, 600, 1900]
+    'horse': [49, 250, 281, 600, 1900],
+    'type': ['ie', 'firefox', 'safari', 'chrome', 'edge']
 }, index=[1990, 1997, 2003, 2009, 2014])
 
 print(f)
 
-f.pdchart.line().render('static/line.html')
-f.pdchart.bar().render('static/bar.html')
-f.pdchart.scatter(effect=True).render('static/scatter.html')
-f.pdchart.pie(y='horse').render('static/pie.html')
+# basic
+# f.pdchart.line(x='type', y=['pig', 'horse']).render('static/line.html')
+# f.pdchart.bar().render('static/bar.html')
+# f.pdchart.scatter(effect=True).render('static/scatter.html')
+# f.pdchart.pie(label='horse').render('static/pie.html')
+# f.pdchart.wordCloud(label='type', value='pig').render('static/wordCloud.html')
+
+
+# grid
+# line = f.pdchart.line(x='type', y=['pig', 'horse'],
+#                       init_opts={'theme': ThemeType.LIGHT},
+#                       y_opts={'pig': {'is_smooth': True}, 'horse': {'is_step': True}})
+# bar = f.set_index('type').pig.pdchart.bar(global_opts={'title_opts': opts.TitleOpts(title="Grid-Line", pos_top="48%"),
+#                                                        'legend_opts': opts.LegendOpts(pos_top="48%")})
+# grid = (
+#     Grid()
+#         .add(bar, grid_opts=opts.GridOpts(pos_bottom="60%"))
+#         .add(line, grid_opts=opts.GridOpts(pos_top="60%"))
+# )
+# grid.render('static/grid.html')
+
+
+f = pd.DataFrame({
+    'year': [str(i) + '年' for i in
+             [2001, 2002, 2003, 2004, 2001, 2002, 2003, 2004, 2001, 2002, 2003, 2004, 2001, 2002, 2003, 2004]],
+    'horse': [490, 250, 271, 650, 190, 250, 289, 600, 290, 250, 181, 208, 190, 250, 291, 390],
+    'pig': [190, 350, 278, 450, 290, 290, 289, 600, 390, 150, 181, 258, 290, 550, 391, 280],
+    'type': ['firefox', 'firefox', 'firefox', 'firefox', 'safari', 'safari', 'safari', 'safari', 'chrome', 'chrome',
+             'chrome', 'chrome', 'edge', 'edge', 'edge', 'edge']
+})
+
+# print(f)
+# f.pdchart.bar(x='type', y=['horse', 'pig'], timeline='year').render('static/pie.html')
+
+# f.pdchart.pie(label='type', value='horse', timeline='year',
+#               val_opts={'rosetype': 'radius', 'radius': ["30%", "55%"]}).render('static/pie.html')
+
+import matplotlib
+
+# df = pd.DataFrame({
+#     'sum': [100, 380, 289, 460, 150],
+#     'size': [123, 180, 250, 90, 150],
+#     'year': [1990, 1994, 1998, 2000, 2004],
+#     'type': ['ie', 'firefox', 'safari', 'chrome', 'edge']
+# })
+# df.pdchart.line(x='year', y=['sum', 'size'],
+#                 init_opts={'theme': ThemeType.LIGHT},
+#                 y_opts={'sum':{'is_smooth':True}, 'size':{'is_step':True}}).render('static/line.html')
+
+words = [
+    ("Sam S Club", 10000),
+    ("Macys", 6181),
+    ("Amy Schumer", 4386),
+    ("Jurassic World", 4055),
+    ("Charter Communications", 2467),
+    ("Chick Fil A", 2244),
+    ("Planet Fitness", 1868),
+    ("Pitch Perfect", 1484),
+    ("Express", 1112),
+    ("Home", 865),
+    ("Johnny Depp", 847),
+    ("Lena Dunham", 582),
+    ("Lewis Hamilton", 555),
+    ("KXAN", 550),
+    ("Mary Ellen Mark", 462),
+    ("Farrah Abraham", 366),
+    ("Rita Ora", 360),
+    ("Serena Williams", 282),
+    ("NCAA baseball tournament", 273),
+    ("Point Break", 265),
+]
+
+new_df = pd.DataFrame(words, columns=['name', 'num'])
+print(new_df)
--- pdchart.py
+++ pdchart.py
@@ -1,10 +1,142 @@
 # 2019-05-30
+from fnmatch import fnmatch
+
 import pandas as pd
 from pyecharts import charts
+from pyecharts import options as opts
 
 
-@pd.api.extensions.register_dataframe_accessor("pdchart")
-class PdChartAccessor(object):
+class BaseAccessor(object):
+    def __init__(self, pandas_obj):
+        self.df = pandas_obj
+        self.chart_xy = {}
+        self.chart_basic = {}
+
+    def plot_xy(self, x=None, y=None, kind='line', mdf=None, **kwargs):
+        """
+        直角坐标系图表
+        :param x: 用于指定x轴标签，默认为dataframe的index
+        :param y: 用于指定y轴数据，支持str、list、tuple，默认整个为dataframe
+        :param kind: 图表类型
+        :param mdf: 默认self.df, 可以传自定义的frame
+        :param kwargs: 配置选项
+        :return:
+        """
+        if kind not in self.chart_xy:
+            raise NotImplementedError
+
+        if mdf is None:
+            mdf = self.df
+
+        # 获取配置
+        init_opts = kwargs.get('init_opts', {})
+        y_opts = kwargs.get('y_opts', {})
+        default_global_opts = {'title_opts': {'text': kind.title()}, 'toolbox_opts': opts.ToolboxOpts(is_show=True)}
+        global_opts = kwargs.get('global_opts', default_global_opts)
+
+        # 初始化图表
+        c = self.chart_xy.get(kind)(opts.InitOpts(**init_opts))
+
+        # x轴标签
+        x_data = mdf[x].astype('str').tolist() if x else mdf.index.astype('str').tolist()
+        c = c.add_xaxis(x_data)
+
+        # y轴值
+        y_data = mdf
+        if isinstance(y, (list, tuple)):
+            y_data = mdf[y]
+        elif isinstance(y, str):
+            y_data = mdf[[y]]
+
+        for k, col in y_data.iteritems():
+            # 支持通配符的模式匹配配置选项
+            _opts = {}
+            for opts_key, opts_val in y_opts.items():
+                if fnmatch(k, opts_key):
+                    _opts.update(opts_val)
+            # 添加y轴的值
+            c.add_yaxis(k, col.tolist(), **_opts)
+
+        # 默认配置
+        c = c.set_global_opts(**global_opts)
+
+        return c
+
+    def plot_basic(self, label=None, value=None, kind='pie', mdf=None, **kwargs):
+        # 基本图表
+        if kind not in self.chart_basic:
+            raise NotImplementedError
+
+        if mdf is None:
+            mdf = self.df
+
+        # 获取配置
+        init_opts = kwargs.get('init_opts', {})
+        val_opts = kwargs.get('val_opts', {})
+        default_global_opts = {'title_opts': {'text': kind.title()}, 'toolbox_opts': opts.ToolboxOpts(is_show=True)}
+        global_opts = kwargs.get('global_opts', default_global_opts)
+
+        # 初始化图表
+        c = self.chart_basic.get(kind)(opts.InitOpts(**init_opts))
+
+        # x default is self.df.index,
+        label_data = mdf.index.astype('str').tolist() if label is None else mdf[label].astype('str').tolist()
+
+        # y default is self.df first column
+        value_data = mdf.iloc[:, 0].tolist() if value is None else mdf[value].tolist()
+
+        c = c.add('', [list(item) for item in zip(label_data, value_data)], **val_opts)
+
+        # default setting
+        c = c.set_global_opts(**global_opts)
+
+        # pie setting
+        # c = c.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+
+        return c
+
+    def plot_func(self, func, *args, **kwargs):
+        timeline = kwargs.pop('timeline') if 'timeline' in kwargs else None
+
+        if timeline:
+            timeline_init_opts = kwargs.get('timeline_init_opts', {})
+            timeline_schema = kwargs.get('timeline_schema', {})
+            tl = charts.Timeline(**timeline_init_opts)
+            tl.add_schema(**timeline_schema)
+
+            for k, _frame in self.df.groupby(timeline):
+                _frame = _frame.drop(columns=timeline)
+                tl.add(func(mdf=_frame, *args, **kwargs), k)
+            return tl
+
+        return func(*args, **kwargs)
+
+    def line(self, x=None, y=None, **kwargs):
+        # 折线图
+        return self.plot_func(self.plot_xy, x=x, y=y, kind='line', **kwargs)
+
+    def bar(self, x=None, y=None, **kwargs):
+        # 柱状图
+        return self.plot_func(self.plot_xy, x=x, y=y, kind='bar', **kwargs)
+
+    def scatter(self, x=None, y=None, effect=False, **kwargs):
+        # 涟漪散点图 & 散点图
+        if effect:
+            return self.plot_func(self.plot_xy, x=x, y=y, kind='effectScatter', **kwargs)
+        else:
+            return self.plot_func(self.plot_xy, x=x, y=y, kind='scatter', **kwargs)
+
+    def pie(self, label=None, value=None, **kwargs):
+        # 饼图
+        return self.plot_func(self.plot_basic, label=label, value=value, kind='pie', **kwargs)
+
+    def wordCloud(self, label=None, value=None, **kwargs):
+        # 词云图
+        return self.plot_func(self.plot_basic, label=label, value=value, kind='wordCloud', **kwargs)
+
+
+@pd.api.extensions.register_dataframe_accessor('pdchart')
+class PdChartFrameAccessor(BaseAccessor):
     def __init__(self, pandas_obj):
         self.df = pandas_obj
 
@@ -16,59 +148,28 @@ class PdChartAccessor(object):
         }
 
         self.chart_basic = {
-            'pie': charts.Pie
+            'pie': charts.Pie,
+            'wordCloud': charts.WordCloud
         }
 
-    def plot_xy(self, kind='line'):
-        if kind not in self.chart_xy:
-            raise NotImplementedError
+        super(BaseAccessor, self).__init__()
 
-        # init chart
-        c = self.chart_xy.get(kind)()
 
-        c = c.add_xaxis(self.df.index.astype('str').tolist())
+@pd.api.extensions.register_series_accessor('pdchart')
+class PdChartSeriesAccessor(BaseAccessor):
+    def __init__(self, pandas_obj):
+        self.df = pandas_obj.to_frame()
 
-        for k, col in self.df.iteritems():
-            c.add_yaxis(k, col.tolist())
+        self.chart_xy = {
+            'line': charts.Line,
+            'bar': charts.Bar,
+            'scatter': charts.Scatter,
+            'effectScatter': charts.EffectScatter,
+        }
 
-        # default setting
-        c = c.set_global_opts(title_opts={"text": kind.title()})
+        self.chart_basic = {
+            'pie': charts.Pie,
+            'wordCloud': charts.WordCloud
+        }
+        super(BaseAccessor, self).__init__()
 
-        return c
-
-    def plot_basic(self, x=None, y=None, kind='pie'):
-        if kind not in self.chart_basic:
-            raise NotImplementedError
-
-        # init chart
-        c = self.chart_basic.get(kind)()
-
-        # x default is self.df.index, y default is self.df first column
-        x = self.df.index.astype('str').tolist() if x is None else self.df[x].tolist()
-
-        # TODO 如果优雅地获取第一列数据
-        y = self.df.T.head(1).values[0].tolist() if y is None else self.df[y].tolist()
-
-        c = c.add('', [list(item) for item in zip(x, y)])
-
-        # default setting
-        c = c.set_global_opts(title_opts={"text": kind.title()})
-
-        # pie setting
-        # c = c.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
-
-        return c
-
-    def line(self):
-        return self.plot_xy(kind='line')
-
-    def bar(self):
-        # TODO 支持拆分 而不是嵌套 参考pandas api
-        return self.plot_xy(kind='bar')
-
-    def scatter(self, effect=False):
-        # 散点图 & 涟漪散点图
-        return self.plot_xy(kind='effectScatter') if effect else self.plot_xy(kind='scatter')
-
-    def pie(self, x=None, y=None):
-        return self.plot_basic(x=x, y=y, kind='pie')
